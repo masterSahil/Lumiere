b413463720b23d03e010244cf25e6fa51e3c9f60
masterSahil <7882mastersahil@gmail.com>
2026-05-25 10:25:36 +0530

    RBA

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
+'use client'
+import AdminDashboard from "@/pages/Dashboard";
 import LumiereDining from "@/pages/Home";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [role, setRole] = useState('user');
+
+  const VerifyLogin = async () => {
+    const token = sessionStorage.getItem("token");
+
+    try {
+      const res = await axios.get("/api/auth/verify", {
+        headers: {
+          Authorization: `bearer ${token}`
+        }
+      })
+
+      setRole(res.data.role);
+    } catch (error) {
+      console.log(error);
+    } 
+  }
+
+  useEffect(() => {
+    VerifyLogin();
+  }, [])
 
   return (
     <main>
-      <LumiereDining />
+      { role === 'user' ? <LumiereDining /> : <AdminDashboard /> }
     </main>
   );
 }
