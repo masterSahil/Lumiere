f8881cad73118a01bc246e8866f6996ce716bd2d
masterSahil <7882mastersahil@gmail.com>
2026-07-03 16:51:21 +0530

    further updated

--- middleware.js
+++ middleware.js
@@ -58,13 +58,6 @@ export async function middleware(request) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
     "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
